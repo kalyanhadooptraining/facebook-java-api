022c41e65293510f244fb96150f0570bad72a4da
fernman <fernman@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2008-08-30 21:42:45 +0000

    CREATE:BRANCH: fbapi-2.0.1 from branches/fbapi-2.0.0@293

---
+++ branches/fbapi-2.0.1/facebook-java-api/src/java/com/facebook/api/Facebook.java
@@ -0,0 +1,21 @@
+package com.facebook.api;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.w3c.dom.Document;
+
+/**
+ * Utility class to handle authorization and authentication of requests. Objects of this class are meant to be created for every request. They are stateless and are not
+ * supposed to be kept in the session.
+ * 
+ * @deprecated deprecated, renaming to FacebookWebappHelper; this implementation also uses deprecated FacebookRestClient
+ */
+@Deprecated
+public class Facebook extends FacebookWebappHelper<Document> {
+
+	public Facebook( HttpServletRequest request, HttpServletResponse response, String apiKey, String secret ) {
+		super( request, response, apiKey, secret, new FacebookRestClient( apiKey, secret ) );
+	}
+
+}
