9ad223acfe2fc092ae08f44ef186401597087f0e
fernman <fernman@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2008-10-30 15:11:35 +0000

    ISSUE:120 adding allocation type 'announcement_notifications_per_week'

--- facebook-java-api/src/main/java/com/google/code/facebookapi/AllocationType.java
+++ facebook-java-api/src/main/java/com/google/code/facebookapi/AllocationType.java
@@ -6,8 +6,12 @@ package com.google.code.facebookapi;
  * @see http://wiki.developers.facebook.com/index.php/Admin.getAllocation
  */
 public enum AllocationType {
-	NOTIFICATIONS_PER_DAY("notifications_per_day"), REQUESTS_PER_DAY("requests_per_day"), EMAILS_PER_DAY("emails_per_day"), EMAIL_DISABLE_MESSAGE_LOCATION(
-			"email_disable_message_location");
+
+	NOTIFICATIONS_PER_DAY("notifications_per_day"),
+	ANNOUNCEMENT_NOTIFICATIONS_PER_WEEK("announcement_notifications_per_week"),
+	REQUESTS_PER_DAY("requests_per_day"),
+	EMAILS_PER_DAY("emails_per_day"),
+	EMAIL_DISABLE_MESSAGE_LOCATION("email_disable_message_location");
 
 	private String name;
 
