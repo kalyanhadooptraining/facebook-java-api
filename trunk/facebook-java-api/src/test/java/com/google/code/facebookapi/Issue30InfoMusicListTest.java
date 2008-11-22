44bf292b45a69079f9994faa06238718a968619b
fernman <fernman@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2008-11-22 02:23:44 +0000

    ISSUE:30 simplifying how we deal with returned content, less processing, might break peoples expectations

--- trunk/facebook-java-api/src/test/java/com/google/code/facebookapi/Issue30InfoMusicListTest.java
+++ trunk/facebook-java-api/src/test/java/com/google/code/facebookapi/Issue30InfoMusicListTest.java
@@ -12,13 +12,14 @@ import com.google.code.facebookapi.schema.UsersGetInfoResponse;
 public class Issue30InfoMusicListTest {
 
 	@Test
-	public void testAppInfo() throws Exception {
+	public void test_getUserMusic() throws Exception {
 		IFacebookRestClient<Object> client = FacebookSessionTestUtils.getValidClient( FacebookJaxbRestClient.class );
 		long uid = client.users_getLoggedInUser();
 		Set<CharSequence> fields = new HashSet<CharSequence>( Arrays.asList( "uid", "music" ) );
 		UsersGetInfoResponse response = (UsersGetInfoResponse) client.users_getInfo( Arrays.asList( uid ), fields );
 		User user = response.getUser().get( 0 );
 		System.out.println( "music:" + user.getMusic().getValue() );
+		System.out.println( "music-raw:" + client.getRawResponse() );
 	}
 
 }
