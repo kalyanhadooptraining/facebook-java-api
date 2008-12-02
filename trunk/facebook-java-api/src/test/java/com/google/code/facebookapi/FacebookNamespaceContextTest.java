bb837f77f6b0253e295306ebb19cf99c4c0faefc
fernman <fernman@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2008-12-02 02:51:09 +0000

    MISC: code styling

--- trunk/facebook-java-api/src/test/java/com/google/code/facebookapi/FacebookNamespaceContextTest.java
+++ trunk/facebook-java-api/src/test/java/com/google/code/facebookapi/FacebookNamespaceContextTest.java
@@ -21,17 +21,18 @@ public class FacebookNamespaceContextTest {
 	@Test
 	public void testGetPrefixes() {
 		FacebookNamespaceContext fnc = new FacebookNamespaceContext();
-		Iterator<String> it = fnc.getPrefixes("http://api.facebook.com/1.0/");
-		assertTrue(it.hasNext());
-		assertEquals("fbapi", it.next());
+		Iterator<String> it = fnc.getPrefixes( "http://api.facebook.com/1.0/" );
+		assertTrue( it.hasNext() );
+		assertEquals( "fbapi", it.next() );
 
 		try {
 			it.remove();
-			fail("Iterator should not allow removal of prefixes");
-		} catch (UnsupportedOperationException ex) {
+			fail( "Iterator should not allow removal of prefixes" );
 		}
-
-		assertFalse(it.hasNext());
+		catch ( UnsupportedOperationException ex ) {
+			// empty
+		}
+		assertFalse( it.hasNext() );
 	}
 
 	@Test
@@ -44,17 +45,15 @@ public class FacebookNamespaceContextTest {
 		DocumentBuilder db = dbf.newDocumentBuilder();
 		Document document = db.newDocument();
 
-		Element fqlQueryResponse = document.createElementNS(
-				"http://api.facebook.com/1.0/", "fql_query_response");
-		document.appendChild(fqlQueryResponse);
+		Element fqlQueryResponse = document.createElementNS( "http://api.facebook.com/1.0/", "fql_query_response" );
+		document.appendChild( fqlQueryResponse );
 
 		String xpathexpression = "count(/fbapi:fql_query_response)";
-		String count = xpath.evaluate(xpathexpression, document);
-		assertEquals("No elements will be found because the processor "
-				+ "doesn't yet know about the fbapi prefix", "0", count);
-		xpath.setNamespaceContext(fnc);
-		count = xpath.evaluate(xpathexpression, document);
-		assertEquals("1 element will be found because the processor "
-				+ "now knows about the fbapi prefix", "1", count);
+		String count = xpath.evaluate( xpathexpression, document );
+		assertEquals( "No elements will be found because the processor " + "doesn't yet know about the fbapi prefix", "0", count );
+		xpath.setNamespaceContext( fnc );
+		count = xpath.evaluate( xpathexpression, document );
+		assertEquals( "1 element will be found because the processor " + "now knows about the fbapi prefix", "1", count );
 	}
+
 }
