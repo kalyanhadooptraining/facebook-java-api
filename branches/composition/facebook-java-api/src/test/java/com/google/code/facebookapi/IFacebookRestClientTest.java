46b45d26d3873c02e2b0ed7112a796b3d7183c8d
david.j.boden <david.j.boden@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2008-12-03 14:18:31 +0000

    CREATE: created branches/composition from trunk@455

---
+++ branches/composition/facebook-java-api/src/test/java/com/google/code/facebookapi/IFacebookRestClientTest.java
@@ -0,0 +1,17 @@
+package com.google.code.facebookapi;
+
+import static junit.framework.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class IFacebookRestClientTest {
+
+	@Test
+	public void test_emailHash() {
+		String email = "mary@example.com";
+		String expectedHash = "4228600737_c96da02bba97aedfd26136e980ae3761";
+		String hash = FacebookSignatureUtil.generateEmailHash( email );
+		assertEquals( expectedHash, hash );
+	}
+
+}
