88fe3e457ed14c55083091d2f37a7f3321dacb2b
david.j.boden <david.j.boden@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2008-12-21 18:43:23 +0000

    Issue152 Prove that user logout results in FacebookException being thrown.

---
+++ trunk/facebook-java-api/src/test/java/com/google/code/facebookapi/Issue152UserLogoutTest.java
@@ -0,0 +1,29 @@
+package com.google.code.facebookapi;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class Issue152UserLogoutTest {
+
+	@Test
+	public void testUserLogoutFacebookException() throws FacebookException, IOException  {
+		FacebookXmlRestClient client = FacebookSessionTestUtils.getValidClient( FacebookXmlRestClient.class );
+		
+		client.auth_expireSession();
+		
+		try {
+			client.auth_getSession( FacebookSessionTestUtils.lastTokenUsed );
+			fail("Session is expired, getSession should fail");
+		} catch( FacebookException ex ) {
+			//Success
+			System.out.println("Logout success, FacebookException thrown");
+		}
+		
+		
+		
+	}
+	
+}
