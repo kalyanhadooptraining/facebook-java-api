fdf8846c25719dce601c4a660bbb83e5256d7fb8
david.j.boden <david.j.boden@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2008-12-21 18:15:44 +0000

    Removed com.sun XPathAPI import which was causing me a problem on my Linux Java 1.5 build.

--- facebook-java-api/src/test/java/com/google/code/facebookapi/PhotoAlbumTest.java
+++ facebook-java-api/src/test/java/com/google/code/facebookapi/PhotoAlbumTest.java
@@ -1,10 +1,13 @@
 package com.google.code.facebookapi;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.net.URL;
 import java.util.List;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -15,25 +18,30 @@ import org.w3c.dom.NodeList;
 
 import com.google.code.facebookapi.schema.Album;
 import com.google.code.facebookapi.schema.PhotosGetAlbumsResponse;
-import com.sun.org.apache.xpath.internal.XPathAPI;
 
 public class PhotoAlbumTest {
-
+	
 	@Test
 	public void test_PhotoAlbumList() throws Exception {
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xpath = factory.newXPath();
+		xpath.setNamespaceContext(new FacebookNamespaceContext());
+		
 		FacebookXmlRestClient client = FacebookSessionTestUtils.getValidClient( FacebookXmlRestClient.class );
 		client.setNamespaceAware( false );
 		Long userId = client.users_getLoggedInUser();
 		Document doc = client.photos_getAlbums( userId );
 		assertNotNull( doc );
-		NodeList albums = XPathAPI.selectNodeList( doc, "//album" );
+		//You'd need //fbapi:album if the FacebookXmlRestClient was set to
+	    //namespace aware.
+		NodeList albums = (NodeList)xpath.evaluate( "//album", doc, XPathConstants.NODESET );
 		assertTrue( albums.getLength() > 0 );
 		for ( int i = 0; i < albums.getLength(); i++ ) {
 			Node albumNode = albums.item( i );
-			assertNotNull( XPathAPI.eval( albumNode, "aid/text()" ) );
-			assertNotNull( XPathAPI.eval( albumNode, "name/text()" ) );
-			assertNotNull( XPathAPI.eval( albumNode, "created/text()" ) );
-			String link = XPathAPI.eval( albumNode, "link/text()" ).toString();
+			assertNotSame( "", xpath.evaluate( "aid/text()", albumNode ) );
+			assertNotSame( "", xpath.evaluate( "name/text()", albumNode ) );
+			assertNotSame( "", xpath.evaluate( "created/text()", albumNode ) );
+			String link = xpath.evaluate( "link/text()", albumNode );
 			new URL( link );
 		}
 	}
