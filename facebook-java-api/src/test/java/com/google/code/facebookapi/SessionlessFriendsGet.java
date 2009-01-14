93dbd0c427ec5e6e44a770d5e3f431e6c0d65565
david.j.boden <david.j.boden@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2009-01-14 09:28:02 +0000

    Examine sessionless methods

---
+++ facebook-java-api/src/test/java/com/google/code/facebookapi/SessionlessFriendsGet.java
@@ -0,0 +1,29 @@
+package com.google.code.facebookapi;
+
+import static junit.framework.Assert.*;
+
+import org.json.JSONArray;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+public class SessionlessFriendsGet {
+
+	@Test
+	@Ignore
+	public void testFriendsGetWithoutSession() throws FacebookException {
+		FacebookJsonRestClient client = FacebookSessionTestUtils.getSessionlessValidClient( FacebookJsonRestClient.class );
+
+		try {
+			client.friends_get();
+			fail("Shouldn't be able to use base friends_get() without a session");
+		} catch(FacebookException ex) {
+			//Ignore
+		}
+		
+		long uid = client.users_getLoggedInUser();
+		JSONArray friends = (JSONArray)client.friends_get(uid);
+		
+		assertTrue( friends.length() > 0 );
+	}
+}
