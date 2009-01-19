7578021156ffbac78453bf4bad0a2172c05f55a1
david.j.boden <david.j.boden@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2009-01-19 11:59:43 +0000

    Removed unused import.

--- branches/composition/facebook-java-api/src/main/java/com/google/code/facebookapi/ExtensibleClient.java
+++ branches/composition/facebook-java-api/src/main/java/com/google/code/facebookapi/ExtensibleClient.java
@@ -20,7 +20,6 @@ import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
 import java.util.EnumSet;
 import java.util.Iterator;
