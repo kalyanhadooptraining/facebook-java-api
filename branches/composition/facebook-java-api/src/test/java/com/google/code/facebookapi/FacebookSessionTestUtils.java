06e2e42fed39eb3c727a95aabb084eae99271f0f
david.j.boden <david.j.boden@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2009-01-27 22:16:34 +0000

    Remove the need for opening a browser to authenticate with Facebook. Uses Apache HttpClient.

--- branches/composition/facebook-java-api/src/test/java/com/google/code/facebookapi/FacebookSessionTestUtils.java
+++ branches/composition/facebook-java-api/src/test/java/com/google/code/facebookapi/FacebookSessionTestUtils.java
@@ -2,40 +2,43 @@ package com.google.code.facebookapi;
 
 import java.io.IOException;
 import java.lang.reflect.Constructor;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.prefs.Preferences;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpState;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.params.HttpClientParams;
 import org.w3c.dom.Document;
 
-import com.Ostermiller.util.Browser;
-
 public class FacebookSessionTestUtils {
-
-	private static String APIKEY = System.getProperty( "APIKEY" );
-	private static String SECRET = System.getProperty( "SECRET" );
 	
 	public static String lastTokenUsed;
 
 	public static String getValidSessionID() throws IOException, FacebookException {
-		IFacebookRestClient<Document> client = new FacebookXmlRestClient( APIKEY, SECRET );
+		JUnitProperties properties = new JUnitProperties();
+		IFacebookRestClient<Document> client = new FacebookXmlRestClient( properties.getAPIKEY(), properties.getSECRET() );
 		String token = client.auth_createToken();
 
-		try {
-			URI hitURL = new URI( "http://api.new.facebook.com/login.php?api_key=" + APIKEY + "&v=1.0&auth_token=" + token );
-			// With Java 6, we could use the Desktop API to open the browser
-			// Desktop.getDesktop().browse( hitURL );
-			// For Java 5, we're using a 3rd party utility.
-			Browser.init();
-			Browser.displayURL( hitURL.toString() );
-			Thread.sleep( 2000 ); // Give the token registration a 2 second headstart
-		}
-		catch ( InterruptedException ex ) {
-			throw new RuntimeException( "InterruptedException while waiting for browser to open" );
-		}
-		catch ( URISyntaxException ex ) {
-			throw new RuntimeException( "Invalid Facebook URI", ex );
-		}
+		HttpClient http = new HttpClient();
+        http.setParams(new HttpClientParams());
+        http.setState(new HttpState());
+
+        final String LOGIN = "http://www.facebook.com/login.php";
+        
+        GetMethod get = new GetMethod(LOGIN + "?api_key=" + properties.getAPIKEY() + "&v=1.0&auth_token=" + token );
+
+        http.executeMethod(get);
+
+        PostMethod post = new PostMethod(LOGIN);
+        post.addParameter(new NameValuePair("api_key", properties.getAPIKEY()));
+        post.addParameter(new NameValuePair("v", "1.0"));
+        post.addParameter(new NameValuePair("auth_token", token));
+        post.addParameter(new NameValuePair("email", properties.getEMAIL()));
+        post.addParameter(new NameValuePair("pass", properties.getPASS()));
+
+        http.executeMethod(post);
 
 		String sessionID = client.auth_getSession( token );
 		lastTokenUsed = token;
@@ -74,8 +77,9 @@ public class FacebookSessionTestUtils {
 	@SuppressWarnings("unchecked")
 	private static <T extends IFacebookRestClient> T getIFacebookRestClient( Class<T> clientReturnType, String sessionID ) {
 		try {
+			JUnitProperties properties = new JUnitProperties();
 			Constructor<T> clientConstructor = clientReturnType.getConstructor( String.class, String.class, String.class );
-			return clientConstructor.newInstance( APIKEY, SECRET, sessionID );
+			return clientConstructor.newInstance( properties.getAPIKEY(), properties.getSECRET(), sessionID );
 		}
 		catch ( Exception ex ) {
 			throw new RuntimeException( "Couldn't create relevant IFacebookRestClient using reflection", ex );
