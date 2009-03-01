0da330ec849d386cd8afc04ab9f1a8595b56ed94
david.j.boden <david.j.boden@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2009-03-01 17:10:25 +0000

    Issue165 Session Invalid error code has changed to 102. Added check to JUnit test.

--- branches/composition/facebook-java-api/src/test/java/com/google/code/facebookapi/Issue152UserLogoutTest.java
+++ branches/composition/facebook-java-api/src/test/java/com/google/code/facebookapi/Issue152UserLogoutTest.java
@@ -1,6 +1,7 @@
 package com.google.code.facebookapi;
 
 import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 
@@ -19,6 +20,7 @@ public class Issue152UserLogoutTest {
 			fail("Session is expired, getSession should fail");
 		} catch( FacebookException ex ) {
 			//Success
+			assertEquals(ErrorCode.SESSION_INVALID, ex.getCode());
 			System.out.println("Logout success, FacebookException thrown");
 		}
 		
