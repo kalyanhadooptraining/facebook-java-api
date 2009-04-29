ce0fd61b5513484116f0673a19d49af29d34f3fa
fernman <fernman@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2009-04-29 00:28:38 +0000

    ARCH: fixing debug code

--- trunk/facebook-java-api/src/main/java/com/google/code/facebookapi/XMLTestUtils.java
+++ trunk/facebook-java-api/src/main/java/com/google/code/facebookapi/XMLTestUtils.java
@@ -7,20 +7,51 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.w3c.dom.Node;
 
 public class XMLTestUtils {
 
-	public static void print(Node dom) {
+	protected static Log log = LogFactory.getLog( XMLTestUtils.class );
+
+	public static void print( Node dom ) {
 		try {
 			TransformerFactory factory = TransformerFactory.newInstance();
 			Transformer t = factory.newTransformer();
 			StringWriter sw = new StringWriter();
-			t.transform(new DOMSource(dom), new StreamResult(sw));
-			System.out.println(sw.toString());
-		} catch(Exception ex) {
-			System.out.println("Could not print XML document");
+			t.transform( new DOMSource( dom ), new StreamResult( sw ) );
+			System.out.println( sw.toString() );
+		}
+		catch ( Exception ex ) {
+			System.out.println( "Could not print XML document" );
 		}
 	}
-	
+
+	public static void debug( String message, Node dom ) {
+		try {
+			TransformerFactory factory = TransformerFactory.newInstance();
+			Transformer t = factory.newTransformer();
+			StringWriter sw = new StringWriter();
+			t.transform( new DOMSource( dom ), new StreamResult( sw ) );
+			log.debug( message + sw.toString() );
+		}
+		catch ( Exception ex ) {
+			log.warn( message + ": Coult not print XML document", ex );
+		}
+	}
+
+	public static void error( String message, Node dom ) {
+		try {
+			TransformerFactory factory = TransformerFactory.newInstance();
+			Transformer t = factory.newTransformer();
+			StringWriter sw = new StringWriter();
+			t.transform( new DOMSource( dom ), new StreamResult( sw ) );
+			log.error( message + sw.toString() );
+		}
+		catch ( Exception ex ) {
+			log.error( message + ": Coult not print XML document", ex );
+		}
+	}
+
 }
