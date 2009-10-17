a9f258bb08b41f056d544149bd027c2cb3e6419f
fernman <fernman@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2009-10-17 01:25:08 +0000

    ARCH: starting to refactor helper methods into their own classes

---
+++ trunk/facebook-java-api/src/main/java/com/google/code/facebookapi/Pairs.java
@@ -0,0 +1,93 @@
+package com.google.code.facebookapi;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Basic client taking care of rest call mechanics (signing, etc) to facebook. No api knowledge, nor response interpretation is planned.
+ */
+public class Pairs {
+
+	protected static Log log = LogFactory.getLog( Pairs.class );
+
+	protected static Pair<String,CharSequence> newPair( String name, Object value ) {
+		return new Pair<String,CharSequence>( name, String.valueOf( value ) );
+	}
+
+	@Deprecated
+	protected static Pair<String,CharSequence> newPair( String name, boolean value ) {
+		return newPair10( name, value );
+	}
+
+	protected static Pair<String,CharSequence> newPair10( String name, boolean value ) {
+		return newPair( name, value ? "1" : "0" );
+	}
+
+	protected static Pair<String,CharSequence> newPairTF( String name, boolean value ) {
+		return newPair( name, value ? "true" : "false" );
+	}
+
+	protected static Pair<String,CharSequence> newPair( String name, CharSequence value ) {
+		return new Pair<String,CharSequence>( name, value );
+	}
+
+	protected static Pair<String,CharSequence> newPair( String name, Long value ) {
+		return new Pair<String,CharSequence>( name, Long.toString( value ) );
+	}
+
+	protected static Pair<String,CharSequence> newPair( String name, Integer value ) {
+		return new Pair<String,CharSequence>( name, Integer.toString( value ) );
+	}
+
+	protected static boolean addParam( String name, Long value, Collection<Pair<String,CharSequence>> params ) {
+		params.add( newPair( name, value ) );
+		return true;
+	}
+
+	protected static boolean addParamIfNotBlank( String name, Long value, Collection<Pair<String,CharSequence>> params ) {
+		if ( value != null ) {
+			return addParam( name, value, params );
+		}
+		return false;
+	}
+
+	protected static boolean addParamSecondsIfNotBlank( String name, Date value, Collection<Pair<String,CharSequence>> params ) {
+		if ( value != null ) {
+			return addParam( name, value.getTime() / 1000, params );
+		}
+		return false;
+	}
+
+	protected static boolean addParamIfNotBlankZero( String name, Long value, Collection<Pair<String,CharSequence>> params ) {
+		if ( value != null && value.longValue() != 0 ) {
+			return addParam( name, value, params );
+		}
+		return false;
+	}
+
+	protected static boolean addParamDelimitIfNotBlankEmpty( String name, Iterable<?> value, Collection<Pair<String,CharSequence>> params ) {
+		return addParamIfNotBlank( name, BasicClientHelper.delimit( value ), params );
+	}
+
+	protected static boolean addParam( String name, Object value, Collection<Pair<String,CharSequence>> params ) {
+		params.add( newPair( name, value ) );
+		return true;
+	}
+
+	protected static boolean addParam( String name, CharSequence value, Collection<Pair<String,CharSequence>> params ) {
+		params.add( newPair( name, value ) );
+		return true;
+	}
+
+	protected static boolean addParamIfNotBlank( String name, CharSequence value, Collection<Pair<String,CharSequence>> params ) {
+		if ( ( value != null ) && ( !"".equals( value ) ) ) {
+			params.add( newPair( name, value ) );
+			return true;
+		}
+		return false;
+	}
+
+}
