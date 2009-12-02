4b30c1631cca1e994899fe6ebb399b15858ebd2f
fernman <fernman@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2009-12-02 01:18:49 +0000

    a little more generic action links

--- facebook-java-api/src/main/java/com/google/code/facebookapi/SpecificReturnTypeAdapter.java
+++ facebook-java-api/src/main/java/com/google/code/facebookapi/SpecificReturnTypeAdapter.java
@@ -498,7 +498,7 @@ public abstract class SpecificReturnTypeAdapter<T> extends BaseAdapter implement
 		return getClient().sms_sendMessageWithSession( userId, message );
 	}
 
-	public String stream_publish( final String message, final Attachment attachment, final List<BundleActionLink> actionLinks, final Long targetId, final Long userId )
+	public String stream_publish( final String message, final Attachment attachment, final Collection<BundleActionLink> actionLinks, final Long targetId, final Long userId )
 			throws FacebookException {
 		return getClient().stream_publish( message, attachment, actionLinks, targetId, userId );
 	}
