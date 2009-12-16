2d0005db2fb34be93350c7fc99a2726c5428f983
fernman <fernman@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2009-12-16 01:05:23 +0000

    comments

---
+++ facebook-java-api/src/main/java/com/google/code/facebookapi/FacebookWebFilter.java
@@ -0,0 +1,43 @@
+package com.google.code.facebookapi;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FacebookWebFilter implements Filter {
+
+	@SuppressWarnings("unused")
+	private FilterConfig config;
+
+	public void init( FilterConfig filterConfig ) throws ServletException {
+		this.config = filterConfig;
+	}
+
+	public void destroy() {
+		// empty
+	}
+
+	public void doFilter( ServletRequest request, ServletResponse response, FilterChain chain ) throws IOException, ServletException {
+		if ( request instanceof HttpServletRequest && response instanceof HttpServletResponse ) {
+			doFilter( (HttpServletRequest) request, (HttpServletResponse) response, chain );
+		} else {
+			chain.doFilter( request, response );
+		}
+	}
+
+	public void doFilter( HttpServletRequest request, HttpServletResponse response, FilterChain chain ) throws IOException, ServletException {
+		// MAINTAINING FBSESSION INFORMATION:
+		// 3 sources: FBRequestParams, FBConnectCookies, sessionObj
+		// Values can be in requestScope or sessionScope
+
+		// MAINTAINING JSESSIONID COOKIE sync across FBML/BROWSER cookies
+	}
+
+}
