0bb0b731663fed176d646a16eab90c822b4eb19c
fernman <fernman@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2009-12-16 17:43:39 +0000

    rename

--- trunk/facebook-java-api/src/main/java/com/google/code/facebookapi/FBWebFilter.java
+++ trunk/facebook-java-api/src/main/java/com/google/code/facebookapi/FBWebFilter.java
@@ -11,7 +11,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class FacebookWebFilter implements Filter {
+public class FBWebFilter implements Filter {
 
 	private String apiKey;
 	private String secret;
