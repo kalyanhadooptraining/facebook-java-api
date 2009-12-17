36968ff3f4c14a87e62f3db4fb66efa39fd04302
fernman <fernman@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2009-12-17 23:27:01 +0000

    cleanup

--- facebook-java-api/src/main/java/com/google/code/facebookapi/FacebookNamespaceContext.java
+++ facebook-java-api/src/main/java/com/google/code/facebookapi/FacebookNamespaceContext.java
@@ -10,9 +10,7 @@ import javax.xml.namespace.NamespaceContext;
 /**
  * This NamespaceContext provides the namespaces used by Facebook.
  * 
- * Use xpath.setNamespaceContext(new FacebookNamespaceContext()) on your
- * javax.xml.xpath.XPath object so that you can evaluate XPath expressions using
- * prefixes like:
+ * Use xpath.setNamespaceContext(new FacebookNamespaceContext()) on your javax.xml.xpath.XPath object so that you can evaluate XPath expressions using prefixes like:
  * 
  * xpath.evaluate("fbapi:fql_query_response", document);
  * 
@@ -20,32 +18,29 @@ import javax.xml.namespace.NamespaceContext;
  */
 public class FacebookNamespaceContext implements NamespaceContext {
 
-	private static final int NUMBER_OF_NAMESPACES_HINT = 2;
-	private static Map<String, String> prefixToNamespace = new HashMap<String, String>(
-			NUMBER_OF_NAMESPACES_HINT);
-	private static Map<String, String> namespaceToPrefix = new HashMap<String, String>(
-			NUMBER_OF_NAMESPACES_HINT);
+	private static Map<String,String> prefixToNamespace = new HashMap<String,String>();
+	private static Map<String,String> namespaceToPrefix = new HashMap<String,String>();
+
 	static {
-		add("fb", "http://apps.facebook.com/ns/1.0");
-		add("fbapi", "http://api.facebook.com/1.0/");
+		add( "fb", "http://apps.facebook.com/ns/1.0" );
+		add( "fbapi", "http://api.facebook.com/1.0/" );
 	}
 
-	private static void add(String prefix, String namespace) {
-		prefixToNamespace.put(prefix, namespace);
-		namespaceToPrefix.put(namespace, prefix);
+	private static void add( String prefix, String namespace ) {
+		prefixToNamespace.put( prefix, namespace );
+		namespaceToPrefix.put( namespace, prefix );
 	}
 
-	public String getNamespaceURI(String prefix) {
-		return prefixToNamespace.get(prefix);
+	public String getNamespaceURI( String prefix ) {
+		return prefixToNamespace.get( prefix );
 	}
 
-	public String getPrefix(String namespaceURI) {
-		return namespaceToPrefix.get(namespaceURI);
+	public String getPrefix( String namespaceURI ) {
+		return namespaceToPrefix.get( namespaceURI );
 	}
 
-	public Iterator<String> getPrefixes(String namespaceURI) {
-		return Collections.singletonList(namespaceToPrefix.get(namespaceURI))
-				.iterator();
+	public Iterator<String> getPrefixes( String namespaceURI ) {
+		return Collections.singletonList( namespaceToPrefix.get( namespaceURI ) ).iterator();
 	}
 
 }
