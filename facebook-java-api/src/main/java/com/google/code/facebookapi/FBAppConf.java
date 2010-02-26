dda0006f42fa57f87cc62ccc72ce5a47711f1770
fernman <fernman@829c9b9e-2f3d-0410-b3a9-793b6feb71b1>
2010-02-26 07:41:37 +0000

    cleaning out more FBWeb code, introducing FBAppConf

---
+++ facebook-java-api/src/main/java/com/google/code/facebookapi/FBAppConf.java
@@ -0,0 +1,11 @@
+package com.google.code.facebookapi;
+
+public interface FBAppConf {
+
+	public String getAppId();
+
+	public String getApiKey();
+
+	public String getSecret();
+
+}
---
+++ facebook-java-api/src/main/java/com/google/code/facebookapi/FBAppConfs.java
@@ -0,0 +1,9 @@
+package com.google.code.facebookapi;
+
+public interface FBAppConfs {
+
+	public FBAppConf getConfByApiKey();
+
+	public FBAppConf getConfByAppId();
+
+}
